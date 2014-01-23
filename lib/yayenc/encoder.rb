bbe4e49ae29dc986586d3c4decd7a57deedf1b5e
Chris Lucas <chris@chrisjlucas.com>
2014-01-23 11:19:05 -0700

    initial commit

---
+++ lib/yayenc/encoder.rb
@@ -0,0 +1,93 @@
+require_relative 'part'
+
+module YAYEnc
+  class Encoder
+    SPECIAL_BYTES = [0x00, 0x0A, 0x0D, 0x3D]
+    DEFAULT_PART_SIZE = 500_000 - 1
+    LINE_WIDTH = 128
+
+    def self.encode(src, **opts, &block)
+      new(src, **opts).encode(&block)
+    end
+
+    def initialize(src, **opts)
+      @opts = {}
+
+      if src.is_a?(String)
+        @src_io = File.open(src, 'rb')
+      elsif src.is_a?(IO)
+        @src_io = src
+      else
+        # throw something
+      end
+
+      @src_io.rewind
+
+      @opts[:multi_part] = opts.fetch(:multi_part, false)
+      if @opts[:multi_part]
+        @opts[:part_size] ||= DEFAULT_PART_SIZE
+      else
+        @opts[:part_size] = @src_io.size
+      end
+    end
+
+    def encode(&block)
+      parts = []
+      total_parts = (@src_io.size / @opts[:part_size]).ceil
+      cur_part_num = 1
+      start_byte = 1
+
+      until @src_io.eof?
+        part = encode_part(@src_io.read(@opts[:part_size]))
+        part.part_num = cur_part_num
+        part.part_total = total_parts
+        part.start_byte = start_byte
+        part.end_byte = start_byte + part.size
+
+        block_given? ? block.call(part) : parts << part
+
+        cur_part_num += 1
+        start_byte = part.end_byte + 1
+      end
+      @src_io.close
+
+      parts unless block_given?
+    end
+
+    private
+
+    def encode_part(data)
+      part = Part.new
+      part.line_width = LINE_WIDTH
+      part.size = data.size
+
+      StringIO.open(data) do |data_io|
+        until data_io.eof?
+          line_data = data_io.read(LINE_WIDTH)
+          part.pcrc32 = Zlib.crc32(line_data, part.pcrc32)
+          part << encode_line(line_data).read
+        end
+      end
+
+      part
+    end
+
+    def encode_line(data)
+      enc_io = StringIO.new
+
+      data.each_byte do |byte|
+        byte = (byte + 42) % 256
+        if SPECIAL_BYTES.include?(byte)
+          enc_io.putc 0x3D
+          byte = (byte + 64) % 256
+        end
+
+        enc_io.putc byte
+      end
+
+      enc_io.rewind
+      enc_io
+    end
+
+  end
+end
---
+++ lib/yayenc/part.rb
@@ -0,0 +1,71 @@
+module YAYEnc
+  class Part
+    attr_accessor :lines, :line_width, :size, :name, :pcrc32, :crc32
+    attr_accessor :part_num, :part_total
+    attr_accessor :start_byte, :end_byte
+
+    def initialize
+      @lines = []
+      @pcrc32 = 0
+      @crc32 = 0
+    end
+
+    def <<(line)
+      lines << line
+    end
+
+    def final_part?
+      part_num == part_total
+    end
+
+    def to_s
+      to_io.read
+    end
+
+    def to_io
+      StringIO.new.tap do |sio|
+        sio.write(ybegin << "\n")
+        sio.write(ypart << "\n") if multi_part?
+        lines.each { |line| sio.write(line << "\n") }
+        sio.write(yend << "\n")
+        sio.rewind
+      end
+    end
+
+    private
+
+    def ybegin
+      Array.new.tap do |head|
+        head << '=ybegin'
+        head << "part=#{part_num}" if multi_part?
+        head << "total=#{part_total}" if multi_part?
+        head << "line=#{line_width}"
+        # "The filename must always be the last item on the header line."
+        head << "name=#{name}" unless name.nil?
+      end.join(' ')
+    end
+
+    def ypart
+      Array.new.tap do |line|
+        line << '=ypart'
+        line << "begin=#{start_byte}"
+        line << "end=#{end_byte}"
+      end.join(' ')
+    end
+
+    def yend
+      Array.new.tap do |line|
+        line << '=yend'
+        line << "size=#{size}"
+        line << "part=#{part_num}" if multi_part?
+        line << "pcrc32=#{pcrc32.to_s(16)}" unless pcrc32.zero?
+        line << "crc32=#{crc32.to_s(16)}" unless crc32.zero?
+      end.join(' ')
+    end
+
+    def multi_part?
+      part_total > 1
+    end
+
+  end
+end
