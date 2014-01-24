bca2db0832f74a99fbe06ac9e104a2fd41e545c6
Chris Lucas <chris@chrisjlucas.com>
2014-01-24 10:54:40 -0700

    [spec] more Part::parse tests

--- spec/test_part.rb
+++ spec/test_part.rb
@@ -18,4 +18,39 @@ class TestPartParser < Test::Unit::TestCase
     assert_equal 1,           part.start_byte
     assert_equal 584,         part.end_byte
   end
+
+  def test_part_parser_02
+    parts = YAYEnc::Encoder.encode(file_path('dec2.jpg'),
+                                   line_width: 128,
+                                   part_size: 11250)
+
+
+    YAYEnc::Part.parse(parts[0].to_s).tap do |part|
+      assert_equal 'dec2.jpg',  part.name
+      assert_equal 1,           part.part_num
+      assert_equal 2,           part.part_total
+      assert_equal 1,           part.start_byte
+      assert_equal 11250,       part.end_byte
+      assert_equal 11250,       part.part_size
+      assert_equal 19338,       part.total_size
+      assert_equal 3215875083,  part.pcrc32
+
+      assert part.multi_part?
+      assert !part.final_part?
+    end
+
+    YAYEnc::Part.parse(parts[1].to_s).tap do |part|
+      assert_equal 'dec2.jpg',  part.name
+      assert_equal 2,           part.part_num
+      assert_equal 2,           part.part_total
+      assert_equal 11251,       part.start_byte
+      assert_equal 19338,       part.end_byte
+      assert_equal 8088,        part.part_size
+      assert_equal 19338,       part.total_size
+      assert_equal 2896650307,  part.pcrc32
+
+      assert part.multi_part?
+      assert part.final_part?
+    end
+  end
 end
