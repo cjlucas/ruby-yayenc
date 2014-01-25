6c2ea98d0c2719f0b9eb07859e33d9a8b99f07a0
Chris Lucas <chris@chrisjlucas.com>
2014-01-24 21:34:48 -0700

    [Decoder] added pcrc32 and part_size checks

--- lib/yayenc/decoder.rb
+++ lib/yayenc/decoder.rb
@@ -5,11 +5,12 @@ require 'yayenc/part'
 
 module YAYEnc
   class Decoder
-    def initialize(dest)
+    def initialize(dest, options = {})
       if dest.is_a?(String)
-        raise ArgumentError, 'If dest is a String, it must be a directory path' unless Dir.exists?(dest)
+        raise NotImplementedError, 'String input is not supported'
+        #raise ArgumentError, 'If dest is a String, it must be a directory path' unless Dir.exists?(dest)
 
-        @dest = Pathname.new(dest)
+        #@dest = Pathname.new(dest)
       elsif dest.is_a?(StringIO)
         @dest_io = dest
       else
@@ -17,8 +18,9 @@ module YAYEnc
       end
 
       @opts = {}
-      @opts[:warn] = true
-      @opts[:verify] = true
+      @opts[:verify] = options.fetch(:verify, true)
+      @opts[:warn] = options.fetch(:warn, false)
+      @opts[:debug] = options.fetch(:debug, $DEBUG)
     end
 
     def feed(data)
@@ -42,11 +44,19 @@ module YAYEnc
       esc = false
 
       part.data.each_byte do |byte|
-        next if byte == 0x0A || byte == 0x0D # skip LF/CR
+        next if [0x0A, 0x0D].include?(byte) # skip LF/CR
         esc = true and next if byte == 0x3D && !esc # skip if =
 
         sio.putc(decode_byte(byte, esc))
         esc = false
+      end
+
+      if @opts[:verify]
+        sio.rewind
+        data = sio.read
+
+        logw 'pcrc32 values do not match' unless Zlib.crc32(data, 0) == part.pcrc32
+        logw 'part size does not match' unless data.size == part.part_size
       end
 
       sio.rewind
@@ -58,10 +68,12 @@ module YAYEnc
       byte = (byte - 42) % 256
     end
 
-    private
+    def logw(str)
+      warn str if @opts[:warn]
+    end
 
-    def warn(str)
-      $stderr.write(str << "\r\n") if @opts[:warn]
+    def logd(str)
+      $stderr.write(str << "\r\n") if @opts[:debug]
     end
   end
 end
